2a4859cc2d8961e1329f3cd5420b09ef94bd75a7
Mark Goadrich <goadrich@hendrix.edu>
2024-04-24 15:43:55 -0500

    Configure DB and migration

--- SeatingChart/Program.cs
+++ SeatingChart/Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ChartContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
-
+//builder.Services.AddDbContext<ChartContext>(options =>
+//        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ChartContext>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
